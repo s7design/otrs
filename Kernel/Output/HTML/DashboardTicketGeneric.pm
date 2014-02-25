175c2d197d2578573921adc669b4adb4c319acb8
s7design <otrs@s7designcreative.com>
2014-02-26 00:07:59 +0100

    [Bug 9600] When a ticket is deleted by generic agent job, No such TicketID error
    Set Silent => 1 parameter in TicketGet function, that avoids the error message.

--- Kernel/Output/HTML/DashboardTicketGeneric.pm
+++ Kernel/Output/HTML/DashboardTicketGeneric.pm
@@ -1190,6 +1190,7 @@ sub Run {
             TicketID      => $TicketID,
             UserID        => $Self->{UserID},
             DynamicFields => 0,
+            Silent        => 1
         );
 
         # set a default title if ticket has no title
