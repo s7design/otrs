3279f7a88d383d7c1b71a8bd281c0061c9a26ee6
s7design <otrs@s7designcreative.com>
2015-04-21 17:13:28 +0200

    ported to OM and reorganised Output modules in subdirectories

--- Kernel/Output/HTML/QueuePreferences/QueuePreferencesGeneric.pm
+++ Kernel/Output/HTML/QueuePreferences/QueuePreferencesGeneric.pm
@@ -1,5 +1,5 @@
 # --
-# Kernel/Output/HTML/QueuePreferencesGeneric.pm
+# Kernel/Output/HTML/QueuePreferences/QueuePreferencesGeneric.pm
 # Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
@@ -7,10 +7,15 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::QueuePreferencesGeneric;
+package Kernel::Output::HTML::QueuePreferences::QueuePreferencesGeneric;
 
 use strict;
 use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::System::Web::Request',
+    'Kernel::System::Queue',
+);
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -19,12 +24,6 @@ sub new {
     my $Self = {%Param};
     bless( $Self, $Type );
 
-    # get needed objects
-    for (qw(ConfigObject LogObject DBObject LayoutObject UserID ParamObject ConfigItem QueueObject))
-    {
-        die "Got no $_!" if ( !$Self->{$_} );
-    }
-
     return $Self;
 }
 
@@ -32,7 +31,9 @@ sub Param {
     my ( $Self, %Param ) = @_;
 
     my @Params = ();
-    my $GetParam = $Self->{ParamObject}->GetParam( Param => $Self->{ConfigItem}->{PrefKey} );
+    my $GetParam
+        = $Kernel::OM->Get('Kernel::System::Web::Request')->GetParam( Param => $Self->{ConfigItem}->{PrefKey} );
+
     if ( !defined($GetParam) ) {
         $GetParam = defined( $Param{QueueData}->{ $Self->{ConfigItem}->{PrefKey} } )
             ? $Param{QueueData}->{ $Self->{ConfigItem}->{PrefKey} }
@@ -57,7 +58,7 @@ sub Run {
         for (@Array) {
 
             # pref update db
-            $Self->{QueueObject}->QueuePreferencesSet(
+            $Kernel::OM->Get('Kernel::System::Queue')->QueuePreferencesSet(
                 QueueID => $Param{QueueData}->{QueueID},
                 Key     => $Key,
                 Value   => $_,
