53ece4b674f1422aff59ecd3418208153aa8bfe2
s7design <otrs@s7designcreative.com>
2015-06-01 09:22:22 +0200

    changed name space

--- Kernel/Output/HTML/FilterText/AutoLink.pm
+++ Kernel/Output/HTML/FilterText/AutoLink.pm
@@ -6,7 +6,7 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::FilterText::TextAutoLink;
+package Kernel::Output::HTML::FilterText::AutoLink;
 
 use strict;
 use warnings;
--- Kernel/Output/HTML/FilterText/URL.pm
+++ Kernel/Output/HTML/FilterText/URL.pm
@@ -6,7 +6,7 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::FilterText::TextURL;
+package Kernel::Output::HTML::FilterText::URL;
 
 use strict;
 use warnings;
