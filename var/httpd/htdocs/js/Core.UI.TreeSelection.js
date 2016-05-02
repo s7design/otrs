b6d40576b09be4c8ecf85d01dffd85ddb8a05bb4
s7design <otrs@s7designcreative.com>
2016-05-02 23:43:06 +0200

    updated JS translate strings

--- var/httpd/htdocs/js/Core.UI.TreeSelection.js
+++ var/httpd/htdocs/js/Core.UI.TreeSelection.js
@@ -344,7 +344,7 @@ Core.UI.TreeSelection = (function (TargetNS) {
             Core.UI.Dialog.ShowContentDialog('<div class="OverlayTreeSelector" id="TreeContainer"></div>', DialogTitle, '20%', 'Center', true);
             $('#TreeContainer')
                 .prepend($TreeObj)
-                .prepend('<div id="TreeSearch"><input type="text" id="TreeSearchInput" placeholder="' + Core.Config.Get('SearchMsg') + '..." /><span title="' + Core.Config.Get('DeleteMsg') + '">x</span></div>')
+                .prepend('<div id="TreeSearch"><input type="text" id="TreeSearchInput" placeholder="' + Core.Language.Translate('Search') + '..." /><span title="' + Core.Config.Get('DeleteMsg') + '">x</span></div>')
                 .append('<input type="button" id="SubmitTree" class="Primary" title="' + Core.Config.Get('ApplyButtonText') + '" value="' + Core.Config.Get('ApplyButtonText') + '" />');
         }
         else {
